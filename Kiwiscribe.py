9739d72546c3c03219a923a713200b8c55989ada
fgbkiwi <fgbcasa@gmail.com>
2026-07-20 01:25:57 -0300

    release: Kiwiscribe 1.0.5

--- Kiwiscribe.py
+++ Kiwiscribe.py
@@ -31,7 +31,7 @@ except Exception:
     generate_word_document = None
     DOCX_GENERATOR_AVAILABLE = False
 
-APP_VERSION = "1.0.4"
+APP_VERSION = "1.0.5"
 
 TRANSCRIPTION_MODELS = {
     "AssemblyAI": [("Universal-3 Pro", "universal-3-pro"), ("Universal-2", "universal-2")],
